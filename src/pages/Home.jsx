bb3640c4439ab9bcf52ed44525365aaa3f8b3aa9
Vedant More <morevedant1812@gmail.com>
2022-02-19 10:26:33 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import UserResults from "../components/users/UserResults";
+
+function Home() {
+  return (
+    <>
+      {/* Search components will go here */}
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
+
+//{process.env.REACT_APP_GITHUB_TOKEN}
