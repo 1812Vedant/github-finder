58a5eba5c363f36b86b2401f85ab190078e181a0
Vedant More <morevedant1812@gmail.com>
2022-02-21 05:36:16 +0530

    making clear functionality work

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -43,9 +43,19 @@ export const GithubProvider = ({ children }) => {
   //Set Loading
   const setLoading = () => dispatch({ type: "SET_LOADING" });
 
+  //Clear Users from state
+  const clearUsers = () => {
+    dispatch({ type: "CLEAR_USERS" });
+  };
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, searchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
